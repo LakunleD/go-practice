891b5022cf17bf50a2f72dadb462d28ef67bf50d
Olakunle <OlakunleDosunmu@gmail.com>
2019-11-24 05:39:01 +0100

    switch conditions

--- conditions.go
+++ conditions.go
@@ -13,4 +13,27 @@ func main() {
 	} else {
 		fmt.Println("x is less than 100")
 	}
+
+	y := 2
+
+	switch y {
+	case 1:
+		fmt.Println("y is 1")
+	case 2:
+		fmt.Println("y is 2")
+	case 3:
+		fmt.Println("y is 3")
+	default:
+		fmt.Println("y is unknown")
+	}
+
+	z := 5
+	switch {
+	case z < 1:
+		fmt.Println("z is less than 1")
+	case z > 10:
+		fmt.Println("z is greater than 10")
+	default:
+		fmt.Println("z is between 1 and 10")
+	}
 }
